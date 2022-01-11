2ad8893f318ca81635affbf52c7660594da012c1
pear.cm <pear.cm@traditionasia.com>
2022-01-11 12:01:57 +0700

    train

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const { GraphQLServer, PubSub } = require('graphql-yoga');
 
 let defaultName = "Chi";
+let defaultPetType = "Dog";
+let defaultPetName = "GiGi"
 
 const pubsub = new PubSub();
 
@@ -8,10 +10,12 @@ const typeDefs = `
 	type Query {
 		hello(name: String): String!
 		sayhi: String!
+		displayMyPet(type: String, name: String): String!
 	}
 
 	type Mutation {
 		changeDefaultName(name: String!): String!
+		changeDefaulPetName(name: String!): String!
 	}
 
 	type Subscription {
@@ -21,12 +25,17 @@ const typeDefs = `
 
 const resolvers = {
 	Query: {
-		hello: (root, { name }, ctx, info) => {
+		hello: (root, { name }, ctx, ) => {
 			if (!name)
 				name = defaultName;
 			return `Hello World from ${name}!`;
 		},
-		sayhi: () => "Hi API from graphQL"
+		sayhi: () => "Hi API from graphQL",
+		displayMyPet: (root, { type,  name}, ctx, info) => {
+			if (!type) type = defaultPetType;
+			if (!name) name = defaultPetName;
+			return `My ${type} name is ${name}`;
+		}
 	},
 	Mutation: {
 		changeDefaultName: (root, { name }, ctx, info) => {
@@ -35,6 +44,10 @@ const resolvers = {
 				updateName: `Notify Update Default Name to ${name}`
 			})
 			return `Ok change the default name to ${defaultName}`;
+		},
+		changeDefaulPetName: (root, { name }, ctx, info) => {
+			defaultPetName = name
+			return `Ok change the default name to ${defaultPetName}`;
 		}
 	},
 	Subscription: {
